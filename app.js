c0b5bb27fa3bdde1e064f3cba9ea2ee48ebe1c4c
Edmund Tang <98913678+EdmundLT@users.noreply.github.com>
2022-09-30 13:52:20 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+// index page
+app.get("/", function (req, res) {
+  res.render("pages/index");
+});
+
+// about page
+app.get("/about", function (req, res) {
+  res.render("pages/about");
+});
+
+//contact
+app.get("/contact", function (req, res) {
+  res.render("pages/contact");
+});
+
+//project
+app.get("/project", function (req, res) {
+  res.render("pages/project");
+});
+
+//services
+app.get("/services", function (req, res) {
+  res.render("pages/services");
+});
+app.listen(PORT);
+console.log(`Server is listening on port ${PORT}`);
