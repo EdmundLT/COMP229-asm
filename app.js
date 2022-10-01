61c5c38a52b1d8b880b9d69815291c39cfa57e64
Edmund Tang <98913678+EdmundLT@users.noreply.github.com>
2022-10-01 00:25:42 -0400

    ejs

--- app.js
+++ app.js
@@ -3,16 +3,17 @@
 const express = require("express");
 const app = express();
 const PORT = 8000;
+const bodyParser = require('body-parser');
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 // index page
 app.get("/", function (req, res) {
   res.render("pages/index");
 });
-
+app.use(bodyParser.urlencoded({ extended : false }));
 // about page
 app.get("/about", function (req, res) {
-  res.render("pages/about");
+  res.render("pages/about", {title: "About Me"});
 });
 
 //contact
@@ -20,6 +21,13 @@ app.get("/contact", function (req, res) {
   res.render("pages/contact");
 });
 
+app.post("/form",function(req,res){
+  var email=req.body.email;
+  var message=req.body.message;
+  var form={email, message};
+  console.log(form);
+  res.redirect("/")
+})
 //project
 app.get("/project", function (req, res) {
   res.render("pages/project");
@@ -29,5 +37,6 @@ app.get("/project", function (req, res) {
 app.get("/services", function (req, res) {
   res.render("pages/services");
 });
+
 app.listen(PORT);
 console.log(`Server is listening on port ${PORT}`);
