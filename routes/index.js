9d8cd3a7fcee1fc4df486bcbe01329e46a3d5125
Edmund Tang <98913678+EdmundLT@users.noreply.github.com>
2022-10-07 11:51:10 -0400

    asd

--- routes/index.js
+++ routes/index.js
@@ -2,38 +2,39 @@
 COMP229 Assignment 1
 Student Name: LONG TANG
 SID: 301225866 */
-const express = require("express")
-const router = express.Router()
-const bodyParser = require('body-parser');
+const express = require("express");
+const router = express.Router();
+const bodyParser = require("body-parser");
 router.get("/", function (req, res, next) {
-    res.render("pages/index");
-  });
-  router.use(bodyParser.urlencoded({ extended : false }));
-  // about page
-  router.get("/about", function (req, res, next) {
-    res.render("pages/about", {title: "About Me"});
-  });
-  
-  //contact
-  router.get("/contact", function (req, res, next) {
-    res.render("pages/contact");
-  });
+  res.render("pages/index");
+});
+router.use(bodyParser.urlencoded({ extended: false }));
+// about page
+router.get("/about", function (req, res, next) {
+  res.render("pages/about", { title: "About Me" });
+});
+
+//contact
+router.get("/contact", function (req, res, next) {
+  res.render("pages/contact");
+});
 //Form Submmision
-  router.post("/form",function(req,res, next){
-    var email=req.body.email;
-    var message=req.body.message;
-    var form={email, message};
-    console.log(form)
-    res.send("Successfully submit your message.")
-  })
-  //project
-  router.get("/project", function (req, res, next) {
-    res.render("pages/project");
-  });
-  
-  //services
-  router.get("/services", function (req, res, next) {
-    res.render("pages/services");
-  });
+router.post("/form", function (req, res, next) {
+  var name = req.body.name;
+  var email = req.body.email;
+  var message = req.body.message;
+  var form = { name, email, message };
+  console.log(form);
+  res.redirect("/")
+});
+//project
+router.get("/project", function (req, res, next) {
+  res.render("pages/project");
+});
+
+//services
+router.get("/services", function (req, res, next) {
+  res.render("pages/services");
+});
 
 module.exports = router;
